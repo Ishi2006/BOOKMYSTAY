5f1bb8b616384b4e41767a3361e51f723e698b72
Ishi2006 <ishita.ag2006@gmail.com>
2026-04-01 09:37:29 +0530

    uc1

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,5 +1,64 @@
-public class BookMyStayApp {
-            System.out.println("Welcome to Book My Stay");
-            System.out.println("Hotel Booking Management System v1.0");
+abstract class Room {
+    private String roomType;
+    private int beds;
+    private double price;
 
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 100.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 180.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 300.0);
+    }
+}
+
+public class UseCase2RoomInitialization {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking Management System v2.1\n");
+
+        Room single = new SingleRoom();
+        Room dbl = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("---- Room Details ----\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        dbl.displayDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
 }
